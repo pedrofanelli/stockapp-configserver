ceb2c10e094dc0b6569c1ca6ec0ebef67020f3e6
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-23 19:10:56 -0300

    init

---
+++ src/test/java/com/stockapp/main/StockappConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockapp.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockappConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
