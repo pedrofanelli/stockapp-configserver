1626029ecf0463be2d773de91f1c8fd6754dbdb6
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-28 11:23:38 -0300

    config server working with .env file

--- src/main/java/com/stockapp/main/StockappConfigserverApplication.java
+++ src/main/java/com/stockapp/main/StockappConfigserverApplication.java
@@ -2,8 +2,10 @@ package com.stockapp.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class StockappConfigserverApplication {
 
 	public static void main(String[] args) {
